021b4dae738154f440c1ebbecf269f4322f9621c
Ty Wenrick <tew287@nyu.edu>
2021-12-07 20:15:08 -0500

    working

--- index.js
+++ index.js
@@ -2,10 +2,39 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+//import firebase
+const firebase = require('firebase/app');
+//get config object so we can communicate w firebase
+const firebaseConfig = {
+	apiKey: 'AIzaSyA2AJBUmRoyxWZOaWC9Sw_XU0IoMPq4x30',
+	authDomain: 'final-project-fall-2021-tyw.firebaseapp.com',
+	projectId: 'final-project-fall-2021-tyw',
+	storageBucket: 'final-project-fall-2021-tyw.appspot.com',
+	messagingSenderId: '117157464897',
+	appId: '1:117157464897:web:7607e9d033293780d2efc0',
+	measurementId: '${config.measurementId}',
+};
+
+//Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const indexRoute = require('./routes/index');
+
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,content-type'
+	);
+	next();
 });
 
+app.use('/', indexRoute);
+
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`App Listening on localhost ${port}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+// Require Firestore
+const firestore = require('firebase/firestore');
+// Init Firestore Database
+const db = firestore.getFirestore();
+
+//Get all articles from firebase
+router.get('/', (req, res) => {
+	const users = firestore.getDocs(firestore.collection(db, 'users'));
+
+	const usersArray = [];
+
+	users
+		.then((response) => {
+			response.forEach((doc) => {
+				usersArray.push(doc.data());
+			});
+			return res.send(usersArray);
+		})
+		.catch(function (error) {
+			console.log('Error:', error);
+			return res.send(error);
+		});
+});
+
+module.exports = router;
