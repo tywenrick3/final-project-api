20eedb7e3dc54f01ad297f3c03e99a3bd4c4d415
Ty Wenrick <tew287@nyu.edu>
2021-12-14 13:53:51 -0500

    working

--- routes/profile.js
+++ routes/profile.js
@@ -8,8 +8,6 @@ const db = firestore.getFirestore();
 // Get all articles from a single user firebase
 router.get('/:userId', (req, res) => {
 	const uid = req.params.userId;
-	//const userposts = firestore.getDoc(firestore.doc(db, 'users', userId));
-	//const users = firestore.getDocs(firestore.collection(db, 'users'));
 	const query = firestore.query(
 		firestore.collection(db, 'users'),
 		firestore.where('userId', '==', uid)
@@ -25,24 +23,8 @@ router.get('/:userId', (req, res) => {
 			return res.send(usersArray);
 		})
 		.catch(function (error) {
-			console.log('Error:', error);
 			return res.send(error);
 		});
-
-	// query
-	// 	.then((response) => {
-	// 		response.forEach((doc) => {
-	// 			const docData = doc.data();
-	// 			console.log(docData);
-	// 			docData.id = doc.id;
-	// 			usersArray.push(docData);
-	// 		});
-	// 		return res.send(usersArray);
-	// 	})
-	// 	.catch(function (error) {
-	// 		console.log('Error:', error);
-	// 		return res.send(error);
-	// 	});
 });
 
 module.exports = router;
